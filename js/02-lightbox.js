a0757da24cadf3101d5c7c1f21f8dcb51525be50
reymisterii <104452186+reymisterii@users.noreply.github.com>
2023-03-04 22:46:55 +0200

    Add files via upload

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryImg = document.querySelector(`.gallery`);
+const markup = createMarkeUp(galleryItems);
+
+galleryImg.innerHTML = createMarkeUp(galleryItems);
+
+function createMarkeUp(galleryItems) {
+  const markUp = galleryItems
+    .map(({ preview, original, description}) => {
+    return `
+      <a class="gallery__item"  href="${original}">
+         <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+         title="${description}" 
+         />
+      </a>
+    `;
+  }).join(``);
+
+  return markUp;
+};
+
+let lightbox = new SimpleLightbox('.gallery a', { /* options */ });
